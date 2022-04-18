af058de959d6c51bd65fc26e16f331fef0a74345
Iván Ovejero <ivov.src@gmail.com>
2022-04-18 11:03:38 +0200

    Add rule `node-param-description-comma-separated-hyphen`

---
+++ lib/rules/node-param-description-comma-separated-hyphen.ts
@@ -0,0 +1,50 @@
+import { DOCUMENTATION } from "../constants";
+import * as utils from "../utils";
+import { identifiers as id } from "../utils/identifiers";
+import { getters } from "../utils/getters";
+
+export default utils.createRule({
+  name: utils.getRuleName(module),
+  meta: {
+    type: "layout",
+    docs: {
+      description: `The string \`comma-separated\` in \`description\` must be hyphenated. ${DOCUMENTATION.APPLICABLE_BY_EXTENSION_TO_DESCRIPTION_IN_OPTION}`,
+      recommended: "error",
+    },
+    fixable: "code",
+    schema: [],
+    messages: {
+      hyphenate: "Hyphenate [autofixable]",
+    },
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      ObjectExpression(node) {
+        if (!id.isNodeParameter(node) && !id.isOption(node)) return;
+
+        const description = getters.nodeParam.getDescription(node);
+
+        if (!description) return;
+
+        // ignore first char to disregard casing
+        if (description.value.toLowerCase().includes("omma separated")) {
+          const hyphenated = description.value.replace(
+            /omma separated/g,
+            "omma-separated"
+          );
+
+          const fixed = utils.keyValue("description", hyphenated, {
+            backtickedValue: utils.isMultiline(description),
+          });
+
+          context.report({
+            messageId: "hyphenate",
+            node: description.ast,
+            fix: (fixer) => fixer.replaceText(description.ast, fixed),
+          });
+        }
+      },
+    };
+  },
+});
---
+++ tests/node-param-description-comma-separated-hyphen.test.ts
@@ -0,0 +1,68 @@
+import rule from "../lib/rules/node-param-description-comma-separated-hyphen";
+import { ruleTester, getRuleName } from "../lib/utils";
+import outdent from "outdent";
+
+ruleTester().run(getRuleName(module), rule, {
+  valid: [
+    {
+      code: outdent`
+			const test = {
+				displayName: 'Test',
+				name: 'test',
+				type: 'string',
+				default: '',
+				description: 'A comma-separated list',
+			};`,
+    },
+    {
+      code: outdent`
+			const test = {
+				displayName: 'Test',
+				name: 'test',
+				type: 'string',
+				default: '',
+				description: 'Comma-separated list',
+			};`,
+    },
+  ],
+  invalid: [
+    {
+      code: outdent`
+			const test = {
+				displayName: 'Test',
+				name: 'test',
+				type: 'string',
+				default: '',
+				description: 'A comma separated list',
+			};`,
+      errors: [{ messageId: "hyphenate" }],
+      output: outdent`
+			const test = {
+				displayName: 'Test',
+				name: 'test',
+				type: 'string',
+				default: '',
+				description: 'A comma-separated list',
+			};`,
+    },
+    {
+      code: outdent`
+			const test = {
+				displayName: 'Test',
+				name: 'test',
+				type: 'string',
+				default: '',
+				description: 'Comma separated list',
+			};`,
+      errors: [{ messageId: "hyphenate" }],
+      output: outdent`
+			const test = {
+				displayName: 'Test',
+				name: 'test',
+				type: 'string',
+				default: '',
+				description: 'Comma-separated list',
+			};`,
+    },
+  ],
+});

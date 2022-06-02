3cc2a7db53c4ae0cad52d22eeddfc6c49d48f7e3
Iván Ovejero <ivov.src@gmail.com>
2022-06-02 15:02:05 +0200

    Fix upsert option name

--- lib/rules/node-param-option-name-wrong-for-upsert.ts
+++ lib/rules/node-param-option-name-wrong-for-upsert.ts
@@ -7,13 +7,14 @@ export default utils.createRule({
   meta: {
     type: "layout",
     docs: {
-      description: "Option `name` for Upsert node parameter must be `Upsert`.",
+      description:
+        "Option `name` for Upsert node parameter must be `Create or Update`.",
       recommended: "error",
     },
     fixable: "code",
     schema: [],
     messages: {
-      useUpsert: "Replace with 'Upsert' [autofixable]",
+      useCreateOrUpdate: "Replace with 'Create or Update' [autofixable]",
     },
   },
   defaultOptions: [],
@@ -30,9 +31,9 @@ export default utils.createRule({
 
         if (name.value !== "Upsert") {
           context.report({
-            messageId: "useUpsert",
+            messageId: "useCreateOrUpdate",
             node: name.ast,
-            fix: (fixer) => fixer.replaceText(name.ast, "name: 'Upsert'"),
+            fix: (fixer) => fixer.replaceText(name.ast, "name: 'Create or Update'"),
           });
         }
       },

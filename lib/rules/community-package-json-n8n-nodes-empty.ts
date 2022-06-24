f283b4dadc3d2ec0bd13b35f6ee708e5feb299a9
Iván Ovejero <ivov.src@gmail.com>
2022-06-24 16:59:53 +0200

    Add `community-package-json-n8n-nodes-empty`

---
+++ lib/rules/community-package-json-n8n-nodes-empty.ts
@@ -0,0 +1,68 @@
+import { identifiers as id } from "../utils/identifiers";
+import { getters } from "../utils/getters";
+import * as utils from "../utils";
+import { AST_NODE_TYPES, TSESTree } from "@typescript-eslint/utils";
+
+export default utils.createRule({
+  name: utils.getRuleName(module),
+  meta: {
+    type: "layout",
+    docs: {
+      description:
+        "`n8n.nodes` key must contain at least one filepath in `package.json` of community package",
+      recommended: "error",
+    },
+    fixable: "code",
+    schema: [],
+    messages: {
+      addOneFilepath: "Enter at least one filepath in `n8n.nodes` in package.json",
+    },
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      ObjectExpression(node) {
+        const isTestRun = process.env.NODE_ENV === "test";
+        const isProdRun = !isTestRun;
+        const filename = context.getFilename();
+
+        if (isProdRun && !filename.includes("package.json")) return;
+        if (isProdRun && !id.prod.isTopLevelObjectExpression(node)) return;
+        if (isTestRun && !id.test.isTopLevelObjectExpression(node)) return;
+
+        const n8n = getters.packageJson.getN8n(node);
+
+        if (!n8n) return;
+
+        if (!hasAtLeastOneFilepath(n8n)) {
+          context.report({
+            messageId: "addOneFilepath",
+            node,
+          });
+        }
+      },
+    };
+  },
+});
+
+function hasAtLeastOneFilepath(n8n: { ast: TSESTree.ObjectLiteralElement }) {
+  if (
+    n8n.ast.type === AST_NODE_TYPES.Property &&
+    n8n.ast.value.type === AST_NODE_TYPES.ObjectExpression
+  ) {
+    const nodes = n8n.ast.value.properties.find(id.hasNodesLiteral);
+
+    if (!nodes) return false;
+
+    if (
+      nodes.type === AST_NODE_TYPES.Property &&
+      nodes.value.type === AST_NODE_TYPES.ArrayExpression
+    ) {
+      return nodes.value.elements.some(
+        (element) => element.type === AST_NODE_TYPES.Literal
+      );
+    }
+  }
+
+  return null;
+}

02b497ab02a1b4d078fb3c24cebdc26d3f1d2141
Iván Ovejero <ivov.src@gmail.com>
2022-06-25 12:34:54 +0200

    Improve doc for `nodeClassDescription.identifiers.ts`

--- lib/ast/identifiers/nodeClassDescription.identifiers.ts
+++ lib/ast/identifiers/nodeClassDescription.identifiers.ts
@@ -14,19 +14,39 @@ import type {
 } from "../../types";
 
 /**
- * Module to identify properties in a node class description.
+ * Module to identify properties in a node class `description`.
+ *
+ * ```ts
+ * export class MyService implements INodeType {
+    description: INodeTypeDescription = {
+      displayName: 'My Service',
+      name: 'myService', // ← isName()
+      icon: 'file:myService.svg', // ← isVersion()
+      group: ['transform'],
+      version: 1, // ← isVersion()
+      subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}', // ← isSubtitle()
+      description: 'Consume My Service API',
+      defaults: {
+        name: 'My Service',
+      },
+      inputs: ['main'], // ← isInputs()
+      outputs: ['main'], // ← isOutputs()
+      credentials: [ // ← isCredentials()
+        {
+          name: 'myServiceApi',
+          required: true,
+          testedBy: 'myServiceApiTest',
+        },
+      ],
+      properties: [...] // ← isProperties()
+      ~snip~
+ * ```
  */
 
-export function isVersion(
+export function isName(
   property: TSESTree.ObjectLiteralElement
-): property is NumberProperty {
-  return isNumericPropertyNamed("version", property);
-}
-
-export function isDefaultVersion(
-  property: TSESTree.ObjectLiteralElement
-): property is NumberProperty {
-  return isNumericPropertyNamed("defaultVersion", property);
+): property is StringProperty {
+  return isStringPropertyNamed("name", property);
 }
 
 export function isIcon(
@@ -35,10 +55,22 @@ export function isIcon(
   return isStringPropertyNamed("icon", property);
 }
 
+export function isVersion(
+  property: TSESTree.ObjectLiteralElement
+): property is NumberProperty {
+  return isNumericPropertyNamed("version", property);
+}
+
 export function isSubtitle(
   property: TSESTree.ObjectLiteralElement
 ): property is StringProperty {
   return isStringPropertyNamed("subtitle", property);
+}
+
+export function isDefaultVersion(
+  property: TSESTree.ObjectLiteralElement
+): property is NumberProperty {
+  return isNumericPropertyNamed("defaultVersion", property);
 }
 
 export function isInputs(
@@ -57,12 +89,6 @@ export function isCredentials(
   property: TSESTree.ObjectLiteralElement
 ): property is OptionsProperty {
   return isArrayPropertyNamed("credentials", property);
-}
-
-export function isName(
-  property: TSESTree.ObjectLiteralElement
-): property is StringProperty {
-  return isStringPropertyNamed("name", property);
 }
 
 export function isProperties(

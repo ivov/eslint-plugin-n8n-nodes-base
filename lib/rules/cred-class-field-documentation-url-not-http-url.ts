7fd5444981d6f240901dd7ecba5a51fe291d2ad5
Iván Ovejero <ivov.src@gmail.com>
2022-07-06 14:47:42 +0200

    Add `cred-class-field-documentation-url-not-http-url`

---
+++ lib/rules/cred-class-field-documentation-url-not-http-url.ts
@@ -0,0 +1,62 @@
+import { utils } from "../ast/utils";
+import { id } from "../ast/identifiers";
+import { getters } from "../ast/getters";
+
+const isTestRun = process.env.NODE_ENV === "test";
+
+export default utils.createRule({
+  name: utils.getRuleName(module),
+  meta: {
+    type: "layout",
+    docs: {
+      description:
+        "`documentationUrl` field in credential class must be an HTTP URL. Only applicable to community nodes.",
+      recommended: "error",
+    },
+    schema: [],
+    messages: {
+      useHttpUrl:
+        "Use an HTTP URL, e.g. `https://example.com/docs/auth` [non-autofixable]",
+    },
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      ClassDeclaration(node) {
+        if (
+          !id.isCredentialClass(node) ||
+          !isCommunityCredential(context.getFilename())
+        )
+          return;
+
+        const documentationUrl = getters.credClassBody.getDocumentationUrl(
+          node.body
+        );
+
+        if (!documentationUrl) return;
+
+        if (!isHttpUrl(documentationUrl.value)) {
+          context.report({
+            messageId: "useHttpUrl",
+            node: documentationUrl.ast,
+          });
+        }
+      },
+    };
+  },
+});
+
+function isHttpUrl(string: string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+const isCommunityCredential = (filename: string) =>
+  !filename.includes("packages/credentials") || isTestRun;

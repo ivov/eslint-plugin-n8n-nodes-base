63b5bf63f47b8364c67031d08beb9d3892b0c945
Iván Ovejero <ivov.src@gmail.com>
2022-07-06 11:15:35 +0200

    Add autofix

--- lib/rules/node-execute-block-operation-missing-plural-pairing.ts
+++ lib/rules/node-execute-block-operation-missing-plural-pairing.ts
@@ -2,89 +2,111 @@ import { utils } from "../ast/utils";
 import { getters } from "../ast/getters";
 import { id } from "../ast/identifiers";
 
-// TODO: Mark as Yes/No in `Autofixable` column in README table
-
 const {
-  nodeExecuteBlock: { isPluralPairingStatement, hasValidPluralPairingArgument },
+	nodeExecuteBlock: { isPluralPairingStatement, hasValidPluralPairingArgument },
 } = id;
 
 const {
-  nodeExecuteBlock: { getOperationConsequents, getMarkedNodeFromConsequent },
+	nodeExecuteBlock: { getOperationConsequents, getMarkedNodeFromConsequent },
 } = getters;
 
 export default utils.createRule({
-  name: utils.getRuleName(module),
-  meta: {
-    type: "layout",
-    fixable: "code",
-    docs: {
-      description: `Every \`getAll\` operation in the \`execute()\` method in a node must implement plural pairing:
-      \`\`\`ts
-        returnData.push(
-          ...responseData.map((json) => {
-            return {
-              json,
-              pairedItem: {
-                item: i,
-              },
-            };
-          })
-        );
-        \`\`\`        
-        `,
-      recommended: "error",
-    },
-    schema: [],
-    messages: {
-      missingPluralPairing:
-        "Last statement inside this check is not plural pairing [autofixable]",
-      invalidPluralPairingArgument:
-        "Invalid argument for plural pairing [non-autofixable]",
-    },
-  },
-  defaultOptions: [],
-  create(context) {
-    return {
-      MethodDefinition(node) {
-        if (!utils.isNodeFile(context.getFilename())) return;
+	name: utils.getRuleName(module),
+	meta: {
+		type: "layout",
+		fixable: "code",
+		docs: {
+			description: `Every \`getAll\` operation in the \`execute()\` method in a node must implement plural pairing.`,
+			recommended: "error",
+		},
+		schema: [],
+		messages: {
+			missingPluralPairing:
+				"Last statement inside this check is not plural pairing [autofixable]",
+			invalidPluralPairingArgument:
+				"Invalid argument for plural pairing [non-autofixable]",
+		},
+	},
+	defaultOptions: [],
+	create(context) {
+		return {
+			MethodDefinition(node) {
+				if (!utils.isNodeFile(context.getFilename())) return;
 
-        // plural rule applies to both official nodes and community nodes
+				// plural rule applies to both official nodes and community nodes
 
-        const result = getOperationConsequents(node, { type: "plural" });
+				const result = getOperationConsequents(node, { type: "plural" });
 
-        if (!result) return;
+				if (!result) return;
 
-        const {
-          operationConsequents,
-          returnDataArrayName,
-          inputItemsIndexName,
-        } = result;
+				const {
+					operationConsequents,
+					returnDataArrayName,
+					inputItemsIndexName,
+				} = result;
 
-        for (const opConsequent of operationConsequents) {
-          const lastStatement = opConsequent.body[opConsequent.body.length - 1];
+				for (const opConsequent of operationConsequents) {
+					const lastStatement = opConsequent.body[opConsequent.body.length - 1];
 
-          if (!isPluralPairingStatement(lastStatement, returnDataArrayName)) {
-            context.report({
-              messageId: "missingPluralPairing",
-              node: getMarkedNodeFromConsequent(opConsequent) ?? opConsequent,
-              // TODO: Autofix
-            });
+					if (!isPluralPairingStatement(lastStatement, returnDataArrayName)) {
+						const { range, indentation } = utils.getInsertionArgs({
+							ast: lastStatement,
+						});
 
-            continue;
-          }
+						const pluralPairing = makePluralPairing(
+							indentation,
+							pluralPairingTemplate(returnDataArrayName, inputItemsIndexName)
+						);
 
-          if (
-            !hasValidPluralPairingArgument(lastStatement, inputItemsIndexName)
-          ) {
-            context.report({
-              messageId: "invalidPluralPairingArgument",
-              node: lastStatement.expression,
-            });
+						context.report({
+							messageId: "missingPluralPairing",
+							node: getMarkedNodeFromConsequent(opConsequent) ?? opConsequent,
+							fix: (fixer) => fixer.insertTextAfterRange(range, pluralPairing),
+						});
 
-            continue;
-          }
-        }
-      },
-    };
-  },
+						continue;
+					}
+
+					if (
+						!hasValidPluralPairingArgument(lastStatement, inputItemsIndexName)
+					) {
+						context.report({
+							messageId: "invalidPluralPairingArgument",
+							node: lastStatement.expression,
+						});
+
+						continue;
+					}
+				}
+			},
+		};
+	},
 });
+
+function makePluralPairing(indentation: string, pluralPairingTemplate: string) {
+	return [
+		"\n",
+		pluralPairingTemplate
+			.trim()
+			.split("\n")
+			.map((line) => `${indentation}${line}`)
+			.join("\n"),
+		"\n",
+	].join("");
+}
+
+const pluralPairingTemplate = (
+	returnDataArrayName: string,
+	inputItemsIndexName: string
+) => `
+${returnDataArrayName}.push(
+	...responseData.map((json) => {
+		return {
+			json,
+			pairedItem: {
+				item: ${inputItemsIndexName},
+			},
+		};
+	})
+);
+`;

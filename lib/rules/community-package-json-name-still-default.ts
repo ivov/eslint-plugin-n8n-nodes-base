c212dcc4bad99c746e1fab82712ddeeb9be863f4
Iván Ovejero <ivov.src@gmail.com>
2022-07-19 12:57:50 +0200

    Update community package json default name linting

--- lib/rules/community-package-json-name-still-default.ts
+++ lib/rules/community-package-json-name-still-default.ts
@@ -3,6 +3,9 @@ import { getters } from "../ast/getters";
 import { utils } from "../ast/utils";
 import { COMMUNITY_PACKAGE_JSON } from "../constants";
 
+const isTestRun = process.env.NODE_ENV === "test";
+const isProdRun = !isTestRun;
+
 export default utils.createRule({
 	name: utils.getRuleName(module),
 	meta: {
@@ -20,7 +23,11 @@ export default utils.createRule({
 	create(context) {
 		return {
 			ObjectExpression(node) {
-				if (!id.isCommunityPackageJson(context.getFilename(), node)) return;
+				const filename = context.getFilename();
+
+				if (isProdRun && !filename.includes("package.json")) return;
+				if (isProdRun && !id.prod.isTopLevelObjectExpression(node)) return;
+				if (isTestRun && !id.test.isTopLevelObjectExpression(node)) return;
 
 				const name = getters.communityPackageJson.getName(node);
 

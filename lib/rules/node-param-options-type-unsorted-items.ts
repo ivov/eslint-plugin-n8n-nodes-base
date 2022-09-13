b8b6ab30881cd049a93bcd280e08deec5e68fe0a
Iván Ovejero <ivov.src@gmail.com>
2022-09-13 09:51:49 +0200

    Revert "Force linter to crash to verify that CI check fails"
    This reverts commit ba8cada05bf64308de9c1e51ef48043285600d38.

--- lib/rules/node-param-options-type-unsorted-items.ts
+++ lib/rules/node-param-options-type-unsorted-items.ts
@@ -27,7 +27,6 @@ export default utils.createRule({
 	create(context) {
 		return {
 			ObjectExpression(node) {
-				throw new Error('Oh no');
 				if (!id.isNodeParameter(node)) return;
 
 				if (!id.nodeParam.isOptionsType(node)) return;

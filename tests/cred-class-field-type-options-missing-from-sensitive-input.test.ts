9ed9a61b934aa730ea2d28c06e8e37bbac91d233
Iván Ovejero <ivov.src@gmail.com>
2022-10-31 12:05:23 +0100

    Add tests

---
+++ tests/cred-class-field-type-options-missing-from-sensitive-input.test.ts
@@ -0,0 +1,131 @@
+import rule from "../lib/rules/cred-class-field-type-options-missing-from-sensitive-input";
+import { getRuleName } from "../lib/ast";
+import outdent from "outdent";
+import { ruleTester } from "../lib/ast/utils/ruleTester";
+
+ruleTester().run(getRuleName(module), rule, {
+	valid: [
+		{
+			code: outdent`
+			export class TestApi implements ICredentialType {
+				name = 'testApi';
+				displayName = 'Test API';
+				documentationUrl = 'zammad';
+				properties: INodeProperties[] = [
+					{
+						displayName: 'API Key',
+						name: 'apiKey',
+						type: 'string',
+						typeOptions: { password: true },
+						default: '',
+						required: true,
+					},
+				];
+			}`,
+		},
+	],
+	invalid: [
+		{
+			code: outdent`
+			export class TestApi implements ICredentialType {
+				name = 'testApi';
+				displayName = 'Test API';
+				documentationUrl = 'test';
+				properties: INodeProperties[] = [
+					{
+						displayName: 'Password',
+						name: 'password',
+						type: 'string',
+						default: '',
+						required: true,
+					},
+				];
+			}`,
+			errors: [{ messageId: "addTypeOptionsPassword" }],
+			output: outdent`
+			export class TestApi implements ICredentialType {
+				name = 'testApi';
+				displayName = 'Test API';
+				documentationUrl = 'test';
+				properties: INodeProperties[] = [
+					{
+						displayName: 'Password',
+						name: 'password',
+						type: 'string',
+						typeOptions: { password: true },
+						default: '',
+						required: true,
+					},
+				];
+			}`,
+		},
+		{
+			code: outdent`
+			export class TestApi implements ICredentialType {
+				name = 'testApi';
+				displayName = 'Test API';
+				documentationUrl = 'test';
+				properties: INodeProperties[] = [
+					{
+						displayName: 'API Key',
+						name: 'apiKey',
+						type: 'string',
+						default: '',
+						required: true,
+					},
+				];
+			}`,
+			errors: [{ messageId: "addTypeOptionsPassword" }],
+			output: outdent`
+			export class TestApi implements ICredentialType {
+				name = 'testApi';
+				displayName = 'Test API';
+				documentationUrl = 'test';
+				properties: INodeProperties[] = [
+					{
+						displayName: 'API Key',
+						name: 'apiKey',
+						type: 'string',
+						typeOptions: { password: true },
+						default: '',
+						required: true,
+					},
+				];
+			}`,
+		},
+		{
+			code: outdent`
+			export class TestApi implements ICredentialType {
+				name = 'testApi';
+				displayName = 'Test API';
+				documentationUrl = 'test';
+				properties: INodeProperties[] = [
+					{
+						displayName: 'Access Token',
+						name: 'accessToken',
+						type: 'string',
+						default: '',
+						required: true,
+					},
+				];
+			}`,
+			errors: [{ messageId: "addTypeOptionsPassword" }],
+			output: outdent`
+			export class TestApi implements ICredentialType {
+				name = 'testApi';
+				displayName = 'Test API';
+				documentationUrl = 'test';
+				properties: INodeProperties[] = [
+					{
+						displayName: 'Access Token',
+						name: 'accessToken',
+						type: 'string',
+						typeOptions: { password: true },
+						default: '',
+						required: true,
+					},
+				];
+			}`,
+		},
+	],
+});

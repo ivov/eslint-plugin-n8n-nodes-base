77aaca3af1e006f4f7ddb1f80fe60f7346551d4a
Iván Ovejero <ivov.src@gmail.com>
2022-12-30 10:15:43 +0100

    Create rule `node-class-description-name-miscased` (#148)
    * Create rule
    
    * Write tests
    
    * Update docs

---
+++ lib/rules/node-class-description-name-miscased.ts
@@ -0,0 +1,47 @@
+import { camelCase } from "camel-case";
+import { utils } from "../ast/utils";
+import { id } from "../ast/identifiers";
+import { getters } from "../ast/getters";
+
+export default utils.createRule({
+	name: utils.getRuleName(module),
+	meta: {
+		type: "problem",
+		docs: {
+			description:
+				"`name` in node class description must be camel cased.",
+			recommended: "error",
+		},
+		fixable: "code",
+		schema: [],
+		messages: {
+			useCamelCase: "Change to camel case [autofixable]",
+		},
+	},
+	defaultOptions: [],
+	create(context) {
+		return {
+			ObjectExpression(node) {
+				if (!id.isNodeClassDescription(node)) return;
+
+				if (!utils.isNodeFile(context.getFilename())) return;
+
+				const name = getters.nodeClassDescription.getName(node);
+
+				if (!name) return;
+
+				const camelCased = camelCase(name.value);
+
+				if (name.value !== camelCased) {
+					const fixed = utils.keyValue("name", camelCased);
+
+					context.report({
+						messageId: "useCamelCase",
+						node: name.ast,
+						fix: (fixer) => fixer.replaceText(name.ast, fixed),
+					});
+				}
+			},
+		};
+	},
+});

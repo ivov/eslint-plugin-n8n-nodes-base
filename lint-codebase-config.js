19e2c333ce6d467a7b9064685226a0d5586f62fd
Iván Ovejero <ivov.src@gmail.com>
2023-04-22 11:29:25 +0200

    Re-fix `lint-codebase command`

--- lint-codebase-config.js
+++ lint-codebase-config.js
@@ -1,17 +1,16 @@
 /**
  * Base config for the `lint-codebase` command.
- * 
- * Implicitly references rules in `.eslintplugin.js` via `eslint-plugin-local`.
  *
  * @type {import('@types/eslint').ESLint.ConfigData}
  */
 module.exports = {
-	root: true,
 	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ["./tsconfig.json"],
-	},
 	plugins: ["eslint-plugin-local"],
+
+	/**
+	 * `eslint-plugin-local` references the `nodes` and `credentials` configs in
+	 * `.eslintplugin.js`, which is based on this project's `dist/index.js`.
+	 * Hence this requires `eslint-plugin-n8n-nodes-base` to be installed.
+	 */
 	extends: ["plugin:eslint-plugin-local/nodes", "plugin:eslint-plugin-local/credentials"],
 };

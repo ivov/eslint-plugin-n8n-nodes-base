79c38e8f96f68682c33e98f2667d08aef4ba06d6
Iván Ovejero <ivov.src@gmail.com>
2023-08-06 14:54:42 +0200

    ci: Fix `lint-codebase` command (#175)
    * ci: Fix `lint-codebase` command
    
    * Fix typo

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -10,25 +10,25 @@ import esbuild from "esbuild";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const rootDir = path.resolve(__dirname, "..");
-const distDir = path.resolve(rootDir, "dist");
+const ROOT_DIR = path.resolve(__dirname, "..");
+const DIST_DIR = path.resolve(ROOT_DIR, "dist");
 
-const tsFiles = await glob(path.resolve(rootDir, "lib/**/*.ts"));
+const tsFiles = await glob(path.resolve(ROOT_DIR, "lib/**/*.ts"));
 
 const filesToTranspile = tsFiles.filter(
 	(f) => !f.endsWith(".d.ts") && !f.endsWith("ruleTester.ts")
 );
 
-shell.rm("-rf", distDir);
+shell.rm("-rf", DIST_DIR);
 shell.mkdir("-p", "dist", "lib");
-shell.cp("index.js", path.resolve("dist", "index.js"));
+shell.cp("index.js", path.resolve("dist", "index.js")); // set up entrypoint to `/dist/index.js`
 
 esbuild
 	.build({
 		platform: "node",
 		format: "cjs",
 		entryPoints: filesToTranspile,
-		outdir: path.resolve(distDir, "lib"),
+		outdir: path.resolve(DIST_DIR, "lib"),
 	})
 	.catch((error) => {
 		console.error("[esbuild] Failed to build plugin", error);
--- scripts/make-dot-eslintplugin-js.mjs
+++ scripts/make-dot-eslintplugin-js.mjs
@@ -1,10 +1,5 @@
 "use strict";
 
-/**
- * Create `.eslintplugin.js` based on `dist/index.js` for use by
- * `lint-codebase-config.js` in the `lint-codebase` command.
- */
-
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -13,19 +8,19 @@ import shell, { ShellString } from "shelljs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const rootDir = path.resolve(__dirname, "..");
-const distDir = path.resolve(rootDir, "dist");
+const ROOT_DIR = path.resolve(__dirname, "..");
+const DIST_DIR = path.resolve(ROOT_DIR, "dist");
 
-const pluginIndexFile = path.resolve(distDir, "index.js");
+const pluginIndexFile = path.resolve(DIST_DIR, "index.js");
 
 shell.cp(pluginIndexFile, ".eslintplugin.js");
 
 const oldContent = shell.cat(".eslintplugin.js");
 const newContent = oldContent.replace(
 	/__dirname, "lib", "rules"/,
-	'__dirname, "dist", "lib", "rules"' // adjust reference based on new (root) location
+	'__dirname, "dist", "lib", "rules"' // adjust reference based on new location (root)
 );
 
 new ShellString(newContent).to(".eslintplugin.js");
 
-console.log("Created .eslintplugin.js");
+console.log("Created .eslintplugin.js at root");

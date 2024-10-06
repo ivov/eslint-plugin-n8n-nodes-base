57549e2d0fecc33b4498bd3bac3901545689ad29
Iván Ovejero <ivov.src@gmail.com>
2024-10-06 20:50:22 +0200

    chore: Remove broken autofix for `node-param-collection-type-unsorted-items`

--- tests/node-param-collection-type-unsorted-items.test.ts
+++ tests/node-param-collection-type-unsorted-items.test.ts
@@ -120,45 +120,6 @@ ruleTester().run(getRuleName(module), rule, {
 			errors: [
 				{ messageId: "sortItems", data: { displayOrder: "A | B | C | D | E" } },
 			],
-			output: outdent`
-			const test = {
-				displayName: 'Test',
-				name: 'test',
-				type: 'collection',
-				default: {},
-				options: [
-					{
-						displayName: 'A',
-						name: 'a',
-						type: 'boolean',
-						default: true,
-					},
-					{
-						displayName: 'B',
-						name: 'b',
-						type: 'boolean',
-						default: true,
-					},
-					{
-						displayName: 'C',
-						name: 'c',
-						type: 'boolean',
-						default: true,
-					},
-					{
-						displayName: 'D',
-						name: 'd',
-						type: 'boolean',
-						default: true,
-					},
-					{
-						displayName: 'E',
-						name: 'e',
-						type: 'boolean',
-						default: true,
-					},
-				],
-			};`,
 		},
 	],
 });
